fe30d012bb9e808a45153fdc99287ae54ee1bde4
Thommzy <tomiwaobeisun@yahoo.com>
2018-05-01 09:13:28 +0100

    changes mADE

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -3,13 +3,33 @@
     .controller("listCtrl", ListController);
 
     function ListController() {
-        this.dummyData = "hello World";
+        var vm = this;
+
+        vm.data = "turtlesData";
     }
 
     var turtlesData =[
         {
             type: "Green Turtle",
-            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg"
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
+        },
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
         }
     ]
 })();
