8c35e0ff58de98efa245330320c1bd925f1ee32e
Thommzy <tomiwaobeisun@yahoo.com>
2018-05-01 09:01:36 +0100

    changes

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -5,4 +5,11 @@
     function ListController() {
         this.dummyData = "hello World";
     }
+
+    var turtlesData =[
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg"
+        }
+    ]
 })();
