87b5581fc5158886f13cfb5203cd2614ff575fd4
Thommzy <tomiwaobeisun@yahoo.com>
2018-05-01 10:25:50 +0100

    changes

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -5,10 +5,54 @@
     function ListController() {
         var vm = this;
 
-        vm.data = "turtlesData";
+        vm.data = turtlesData;
     }
 
     var turtlesData =[
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
+        },
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
+        },
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
+        },
+        {
+            type: "Green Turtle",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
+            locations: "Lagos State",
+            size: "Up to 5.0m and up to 400kg",
+            lifespan: "Over 60 years",
+            diet: "Herbivore",
+            description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Ad aperiam, est fugit harum, ipsa ipsam laborum modi mollitia, necessitatibus " +
+            "quas xquisquam similique tenetur velit. Ad aperiam dolor ipsum quos sit!"
+        },
         {
             type: "Green Turtle",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Oriental_Turtle_Dove.jpg",
